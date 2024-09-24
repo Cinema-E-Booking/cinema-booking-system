ff380a89b935d3d121d0aea0edc81025739f54a7
Adrienne Hicks <aeh85590@uga.edu>
2024-09-23 23:21:32 -0400

    movie class

---
+++ backend/Movie.js
@@ -0,0 +1,10 @@
+class Movie {
+    constructor(title, category, synopsis, picture, trailer, rating) {
+        this.title = title;
+        this.category = category;
+        this.synopsis = synopsis;
+        this.rating = rating;
+        this.picture = picture;
+        this.trailer = trailer;
+    }
+}
