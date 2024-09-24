4c181e879ddf8a5ab47a15d57ddb0cc4b039f5fe
Shresta Jha <shrestajha4@gmail.com>
2024-09-23 20:11:46 -0400

    Updated login/registration

---
+++ script.js
@@ -0,0 +1,7 @@
+const loginForm = document.getElementById('login');
+const registerForm = document.getElementById('register');
+
+function toggleForm() {
+    loginForm.classList.toggle('hidden');
+    registerForm.classList.toggle('hidden');
+}
