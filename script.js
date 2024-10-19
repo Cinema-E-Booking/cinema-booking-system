8948dcedb6a354e4f862a7d1612b1a9fcb9d7052
Adrienne Hicks <aeh85590@uga.edu>
2024-10-18 20:30:40 -0400

    Moving all the css and previous js into the next app. css is in the styles folder and js is in the public folder.

--- script.js
+++
@@ -1,7 +0,0 @@
-const loginForm = document.getElementById('login');
-const registerForm = document.getElementById('register');
-
-function toggleForm() {
-    loginForm.classList.toggle('hidden');
-    registerForm.classList.toggle('hidden');
-}
